1f958f7afda02d22338b627ebf2646af8c486999
Philip Thompson <ptnapoleon@gmail.com>
2020-07-08 14:34:30 -0400

    Replace App html with index.html

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './styles.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <html>
+        <head>
+            <meta charset="utf-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    
+    
+            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous" />
+            <link rel="stylesheet" href="css/styles.css" />
+        </head>
+        <body>
+            <header id="top-image">
+                <h2 class="title">Art Portfolio</h2>
+            </header>
+            <nav class="navbar">
+                <button><a href="gallery.html">Gallery</a></button>
+            </nav>
+            <section>
+                <h2>About Me</h2>
+                <p>This is a place with art.</p>
+            </section>
+        </body>
+    </html>
   );
 }
 
